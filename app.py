af3c8edc1be4c248b77d93120c96360c6ff710bc
SergioGL <67619423+SergioGL8@users.noreply.github.com>
2020-10-05 19:19:20 -0500

    Setting up with SQLAlchemy

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
-# 1. import Flask
-from flask import Flask
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+# 1. import Flask
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+#View all classes that automap found
+Base.classes.keys()
+
+# Save reference to the each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
